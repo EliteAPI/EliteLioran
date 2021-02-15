844b4feeb456363d3c7f736b7cd0b7b8fc78f951
Lucas de Jong <lv.dejong@student.avans.nl>
2021-02-15 12:06:42 +0100

    Initial commit

---
+++ EliteLioran/Core.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+using EliteAPI.Abstractions;
+using EliteAPI.Status.Processor.Abstractions;
+using EliteAPI.Status.Ship.Raw;
+
+using IniParser;
+using IniParser.Model;
+using IniParser.Parser;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace EliteLioran
+{
+    // Core class of our application
+    public class Core
+    {
+        private readonly IEliteDangerousApi _api;
+        private readonly IStatusProcessor _status;
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<Core> _log;
+
+        private readonly FileIniDataParser _parser;
+
+        public Core(ILogger<Core> log, IEliteDangerousApi api, IStatusProcessor status, IConfiguration configuration)
+        {
+            _log = log;
+            _api = api;
+            _status = status;
+            _configuration = configuration;
+
+            _parser = new FileIniDataParser();
+        }
+
+        public async Task Run()
+        {
+            await _api.InitializeAsync();
+            _status.ShipUpdated += async (sender, e) => { await UpdateStatus(e.Ship); };
+
+            await _api.StartAsync();
+        }
+
+        async Task UpdateStatus(RawShip ship)
+        {
+            _log.LogInformation("Updating variables ... ");
+            
+            IniData data = new IniData();
+
+            foreach (var property in ship.GetType().GetProperties())
+            {
+               AddProperty(data, property.Name, property, ship);
+            }
+
+            WriteToIni(data);
+        }
+
+        void AddProperty(IniData data, string name, PropertyInfo property, object instance)
+        {
+            switch (Type.GetTypeCode(property.PropertyType))
+            {
+                case TypeCode.Object:
+                    var value = property.GetValue(instance);
+                    foreach (var propertyInfo in value.GetType().GetProperties())
+                    {
+                        AddProperty(data, $"{name}.{propertyInfo.Name}", propertyInfo, value);
+                    }
+                    break;
+                    
+                default:
+                    data["Status"].AddKey(name, property.GetValue(instance)?.ToString());
+                    break;
+            }
+        }
+        
+        private void WriteToIni(IniData data)
+        {
+            string path = _configuration.GetSection("EliteLioran")["IniPath"];
+
+            try
+            {
+                _parser.WriteFile(path, data);
+                _log.LogInformation("Written to {Path}", path);
+            }
+            catch (Exception ex) { _log.LogWarning(ex, "Could not write to {Path}", path); }
+        }
+    }
+}
---
+++ EliteLioran/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+
+using EliteAPI;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using System.Threading.Tasks;
+
+using EliteLioran;
+
+
+using Valsom.Logging.PrettyConsole;
+using Valsom.Logging.PrettyConsole.Formats;
+using Valsom.Logging.PrettyConsole.Themes;
+using Valsom.Logging.File;
+using Valsom.Logging.File.Formats;
+
+// Build the host for dependency injection
+var host = Host.CreateDefaultBuilder()
+    .ConfigureLogging((context, logger) =>
+    {
+        logger.ClearProviders();
+        logger.SetMinimumLevel(LogLevel.Information);
+        logger.AddPrettyConsole(ConsoleFormats.Default, ConsoleThemes.Code);
+        logger.AddPrettyConsole("EliteLorian", new DirectoryInfo(Directory.GetCurrentDirectory()), FileNamingFormats.Default, FileFormats.Default);
+    })
+
+    .ConfigureServices((context, services) =>
+    {
+        services.AddEliteAPI();
+    })
+
+    .Build();
+
+var core = ActivatorUtilities.CreateInstance<Core>(host.Services);
+
+await core.Run();
+
+await Task.Delay(-1);
